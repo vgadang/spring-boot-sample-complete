73ec52485d348e68dc5dda12eda3cfb97f9a5478
Vikram Gadang <vikram.gadang@teamaol.com>
2014-10-07 11:30:47 -0400

    Added named query method

--- src/main/java/com/aol/demo/jpa/repository/CustomerRepository.java
+++ src/main/java/com/aol/demo/jpa/repository/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.aol.demo.jpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.aol.demo.model.Customer;
@@ -10,6 +11,18 @@ import com.aol.demo.model.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String>{
 
+	/**
+	 * Declared Query
+	 * @param lastName
+	 * @return
+	 */
 	List<Customer> findByLastName(String lastName);
 
+	/**
+	 * Annotated sql query
+	 * @param firstName
+	 * @return
+	 */
+	@Query("select c from Customer c where c.lastName = ?1")
+	List<Customer> findByLastNameUsingSql(String lastName);
 }
